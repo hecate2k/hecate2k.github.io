698c5f9f9736fc8c0e2897eb5bc3d236ee4eede0
hecate2k <56253793+hecate2k@users.noreply.github.com>
2019-11-04 12:42:50 +0200

    add circle draw

--- htML/gyro/gyro.js
+++ htML/gyro/gyro.js
@@ -1,12 +1,26 @@
-document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.2";
+document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.3";
 
 window.addEventListener("deviceorientation",on_gyro_data_uab);
 window.addEventListener("devicemotion",on_acc_data_uab);
+
+function deseneaza(unghi_x,unghi_y){
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2d");
+    var r = 10;
+    context.clearRect(0,0,canvas.width,canvas.height);
+    context.beginPath();
+    var x = unghi_x / 90 * (canvas.width / 2 - r) + canvas.width / 2;
+    var y = unghi_y / 90 * (canvas.height / 2 - r) + canvas.height / 2;
+    
+    context.arc(x,y,r,0,2 * Math.Pi);
+    context.stroke();
+}
 
 function on_gyro_data_uab(e){
     document.getElementById("id_alpha").innerHTML = e.alpha.toFixed(2);
     document.getElementById("id_beta").innerHTML = e.beta.toFixed(2);
     document.getElementById("id_gamma").innerHTML = e.gamma.toFixed(2);
+    deseneaza(e.beta,e.gamma);
 }
 
 function on_acc_data_uab(e){
