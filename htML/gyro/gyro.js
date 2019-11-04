7289b47ce963bd77d3083ae15706e21a3f9b827b
hecate2k <56253793+hecate2k@users.noreply.github.com>
2019-11-04 11:46:51 +0200

    Add files via upload

--- htML/gyro/gyro.js
+++ htML/gyro/gyro.js
@@ -1,7 +1,7 @@
 document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.0";
 
 window.addEventListener("deviceorientation",on_gyro_data_uab);
-window.addEventListener("deviceorientation",on_acc_data_uab);
+window.addEventListener("devicemotion",on_acc_data_uab);
 
 function on_gyro_data_uab(e){
     document.getElementById("id_alpha").innerHTML = e.alpha.toFixed(2);
