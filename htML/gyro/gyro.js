4ada9b3dae0cb51951e694649fec85240e7cfeb9
hecate2k <56253793+hecate2k@users.noreply.github.com>
2019-11-04 11:43:39 +0200

    Gravity

--- htML/gyro/gyro.js
+++ htML/gyro/gyro.js
@@ -1,9 +1,18 @@
 document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.0";
 
 window.addEventListener("deviceorientation",on_gyro_data_uab);
+window.addEventListener("deviceorientation",on_acc_data_uab);
 
 function on_gyro_data_uab(e){
     document.getElementById("id_alpha").innerHTML = e.alpha.toFixed(2);
     document.getElementById("id_beta").innerHTML = e.beta.toFixed(2);
     document.getElementById("id_gamma").innerHTML = e.gamma.toFixed(2);
+}
+
+function on_acc_data_uab(e){
+    var acc = e.accelerationIncludingGravity;
+
+    document.getElementById("id_acc_x").innerHTML = acc.x.toFixed(2);
+    document.getElementById("id_acc_y").innerHTML = acc.y.toFixed(2);
+    document.getElementById("id_acc_z").innerHTML = acc.z.toFixed(2);
 }
