d19e19d36ae5f8fb1c448877d881c2dd7be7fd84
hecate2k <56253793+hecate2k@users.noreply.github.com>
2019-11-22 14:54:35 +0200

    Camera + Geolocation

--- cam/cam.js
+++ cam/cam.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "2019.11.22.1"
+document.getElementById("id_logic_version").innerHTML = "2019.11.22.3"
 
 var video = document.getElementById("id_video")
 var c = {audio : true, video: {facingMode:"environment"}}
@@ -10,4 +10,15 @@ function on_ok_cam_uab(e){
 
 function on_fail_cam_uab(e){
     alert("Esec " + e)
+}
+
+function geo_start(){
+    navigator.geolocation.getCurrentPosition(on_ok_geo_uab,on_fail_geo_uab)
+}
+
+function on_ok_geo_uab(e){
+    document.getElementById("id_geo").innerHTML = e.coords.latitude + " " + e.coords.longitude + " " + e.coords.accuracy
+}
+function on_fail_geo_uab(e){
+    alert("Esec" + e)
 }
