424ffb611dfc0aec6a22eba2ad4df1bfe3f41919
hecate2k <56253793+hecate2k@users.noreply.github.com>
2019-12-02 13:05:55 +0200

    update

--- touch/touch.js
+++ touch/touch.js
@@ -1,11 +1,12 @@
 document.getElementById("id_logic").innerHTML = "Logic version: 2019.12.02.0"
 
 window.addEventListener("touchstart",touch_start_uab);
-window.addEventListener("touchmove",touch_move_uab);
+window.addEventListener("touchmove",touch_move_uab,{passive:false});
 window.addEventListener("touchend",touch_end_uab);
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
+
 
 var canvas_rect = canvas.getBoundingClientRect();
 
@@ -24,8 +25,8 @@ function touch_start_uab(p){
     var t = p.changedTouches;
     for(var i = 0 ; i<t.length;i++){
         var touch_info = [];
-        touch_info.x = t[i].pageX;
-        touch_info.y = t[i].pageY;
+        touch_info.x = t[i].pageX - canvas_rect.left;
+        touch_info.y = t[i].pageY - canvas_rect.top;
         touch_info.id= t[i].identifier;
         touch_info.color = get_random_color();
 
@@ -44,7 +45,6 @@ function touch_start_uab(p){
 
 function touch_move_uab(p){
     var t = p.changedTouches;
-    var index_t = -1;
     for(var i = 0 ; i < t.length ; i++){
         for(var j = 0 ; j < last_position.length ; j++)
             if(last_position[j].id == t[i].identifier){
@@ -52,15 +52,15 @@ function touch_move_uab(p){
                 break;
             }
         context.beginPath();
-        context.moveTo(last_position[index_t].x - canvas_rect.left,last_position[index_t].y - canvas_rect.top);
-        context.lineTo(t[i].pageX,t[i].pageY);
+        context.moveTo(last_position[index_t].x,last_position[index_t].y);
+        context.lineTo(t[i].pageX - canvas_rect.left,t[i].pageY- canvas_rect.top);
         context.strokeStyle = last_position[index_t].color;
         context.fillStyle = last_position[index_t].color;
         context.lineWidth = 20;
         context.fill();
         context.stroke();
-        last_position[index_t].x = t[i].pageX;
-        last_position[index_t].y = t[i].pageY;
+        last_position[index_t].x = t[i].pageX- canvas_rect.left;
+        last_position[index_t].y = t[i].pageY- canvas_rect.top;
     }
 }
 
